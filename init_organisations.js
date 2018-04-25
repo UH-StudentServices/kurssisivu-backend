47cdcc7ab75daa081106447b8ab6ce0836a87a4b
Matti Luukkainen <mluukkai@iki.fi>
2018-04-25 17:04:15 +0300

    logging to file

--- init_organisations.js
+++ init_organisations.js
@@ -2,6 +2,7 @@ async function run(codes) {
   const redisClient = require('./src/util/redis')
   const config = require('./src/config')
   await redisClient.setAsync(config.ORGANIZATIONS_KEY, JSON.stringify(codes))
+  console.log('Organisation codes in cache', codes)
   redisClient.quit()
 }
 
