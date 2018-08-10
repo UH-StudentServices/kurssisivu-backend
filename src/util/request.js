c8097dbd498329f1c6769177ea20f5b9ae3b3892
Matti Luukkainen <mluukkai@iki.fi>
2018-08-10 13:44:23 +0300

    fix

--- src/util/request.js
+++ src/util/request.js
@@ -1,13 +1,11 @@
 const axios = require('axios')
+const fs = require('fs')
+const https = require('https')
 
-const agent = KEY_PATH && CERT_PATH ?
-  new https.Agent({
-    cert: fs.readFileSync(process.env.CERT_PATH, 'utf8'),
-    key: fs.readFileSync(process.env.KEY_PATH, 'utf8'),
-  }) :
-  new https.Agent({
-    rejectUnauthorized: false
-  })
+const agent = new https.Agent({
+  cert: fs.readFileSync(process.env.CERT_PATH, 'utf8'),
+  key: fs.readFileSync(process.env.KEY_PATH, 'utf8'),
+})
 
 const instance = axios.create({
   httpsAgent: agent
