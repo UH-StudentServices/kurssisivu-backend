be79021543c79f73142875aae92e4df5c16cd439
Matti Luukkainen <mluukkai@iki.fi>
2018-08-10 10:00:13 +0300

    use key

--- src/util/request.js
+++ src/util/request.js
@@ -1,8 +1,22 @@
 const axios = require('axios')
 
+const agent = KEY_PATH && CERT_PATH ?
+  new https.Agent({
+    cert: fs.readFileSync(process.env.CERT_PATH, 'utf8'),
+    key: fs.readFileSync(process.env.KEY_PATH, 'utf8'),
+  }) :
+  new https.Agent({
+    rejectUnauthorized: false
+  })
+
+const instance = axios.create({
+  httpsAgent: agent
+})
+
 module.exports = async (url) => {
   try {
-    const response = await axios.get(url)
+    const response = await instance.get(url)
+    console.log(response.data.data)
     return response.data.data
   } catch (e) {
     throw `${e.code} ${e.config.url}`
